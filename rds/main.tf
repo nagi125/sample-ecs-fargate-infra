346b6a2610a3ec0b09232980063e39e08f142050
nagi125 <nagi125.dev@gmail.com>
2021-03-25 22:12:40 +0900

    RDSのCIDR修正

--- rds/main.tf
+++ rds/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "pgsql" {
   from_port   = 5432
   to_port     = 5432
   protocol    = "tcp"
-  cidr_blocks = ["10.0.0.0/16"]
+  cidr_blocks = ["10.10.0.0/16"]
 }
 
 resource "aws_db_subnet_group" "main" {
