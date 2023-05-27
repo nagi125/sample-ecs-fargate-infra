1e1a4b0e4fe688cd19f34df4b870d479c6145578
Satoshi Nozue <nagi125.dev@gmail.com>
2023-05-27 19:39:33 +0900

    tfstateの管理を変更/provider設定の更新

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
-# Provider設定
 provider "aws" {
   region = "ap-northeast-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "tfstate-laravel-cms"
+    key     = "terraform.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
+  }
 }
 
 variable "DB_NAME" {
